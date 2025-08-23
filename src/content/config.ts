b428450e9cb4a9eb39bd15d840580739031ae177
Juan Carlos Candela <jccandelabordera@gmail.com>
2025-08-24 01:06:42 +0200

    Improvements

--- src/content/config.ts
+++ src/content/config.ts
@@ -26,14 +26,14 @@ export const collections = {
   projects,
 };
 
-export class SiteConfig {
-  static postsPerPage = 10;
-  static githubUrl = "https://github.com/jcarloscandela";
-  static xUrl = "https://x.com/jcarloscandela";
-  static linkedinUrl = "https://www.linkedin.com/in/jcarloscandela/";
-  static instagramUrl = "";
-  static siteTitle = "Astro - Shadcn UI Portfolio Template";
-  static siteDescription = "A modern Astro template with Shadcn UI components.";
-  static defaultLanguage = "en";
-  static theme = "light";
-}
+export const SiteConfig = {
+  postsPerPage: 10,
+  githubUrl: "https://github.com/jcarloscandela",
+  xUrl: "https://x.com/jcarloscandela",
+  linkedinUrl: "https://www.linkedin.com/in/jcarloscandela/",
+  instagramUrl: "",
+  siteTitle: "Astro Portfolio",
+  siteDescription: "A modern Astro template with Shadcn UI components.",
+  defaultLanguage: "en",
+  theme: "light",
+};
